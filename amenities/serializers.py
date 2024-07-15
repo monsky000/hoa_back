8091e8f7de0d7be889ba8d8753554161899ee270
Raymond Periabras <monperiabras@gmail.com>
2024-07-15 12:56:49 +0800

    update all amenity schedule serializers.py

--- amenities/serializers.py
+++ amenities/serializers.py
@@ -7,11 +7,16 @@ class AmenitiesSerializers(serializers.ModelSerializer):
     fields = '__all__'
 
 class ReservationsSerializers(serializers.ModelSerializer):
+  first_name = serializers.CharField(source='member.first_name', read_only=True)
+  family_name = serializers.CharField(source='member.family_name', read_only=True)
+  amenity_name = serializers.CharField(source='amenity.amenity_name', read_only=True)
   class Meta:
     model = Reservations
     fields = '__all__'
         
 class MaintenanceSerializers(serializers.ModelSerializer):
+  amenity_name = serializers.CharField(source='amenity.amenity_name', read_only=True)
+  contractor_name = serializers.CharField(source='contractor.contractor_name', read_only=True)
   class Meta:
     model = Maintenance
     fields = '__all__'
--- amenities/urls.py
+++ amenities/urls.py
@@ -2,10 +2,13 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
+#Amenity
     path('amenities/', AmenityView.as_view()),
     path('update-amenity/<int:pk>/', AmenityView.as_view()),
+#Reservations
     path('amenity-reservations/', ReservationView.as_view()),
     path('update-amenity-reservation/<int:pk>/', ReservationView.as_view()),
+#Maintenance
     path('amenity-maintenance/', MaintenanceView.as_view()),
-    path('amenity-maintenance/<int:pk>/', MaintenanceView.as_view()),
+    path('update-amenity-maintenance/<int:pk>/', MaintenanceView.as_view()),
 ]
